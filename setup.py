e157294cdbc719347798236b5b81f98fff2f9b00
Shantanu Raj Verma <vshantanu90@gmail.com>
2025-09-09 18:44:03 +0530

    setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="stegotool",
-    version="2.0",
+    version="1.0.0",
     packages=find_packages(),
     install_requires=[
         "pillow",
@@ -15,5 +15,5 @@ setup(
     },
     license="MIT",
     description="StegoTool - Educational Steganography and Payload Simulator",
-    author="Your Name",
+    author="Shantanu Raj Verma",
 )
