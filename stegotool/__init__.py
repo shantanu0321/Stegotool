71b86eba164046278b2eb77f97916e50e66f8386
Shantanu Raj Verma <vshantanu90@gmail.com>
2025-09-10 09:35:45 +0530

    __init__.py

--- stegotool/__init__.py
+++ stegotool/__init__.py
@@ -1,19 +1,17 @@
 # stegotool/__init__.py
 
 """
-StegoTool v2
+StegoTool v1
 Educational steganography & payload demonstration tool.
 
 Modules:
 - cli: Command-line interface
 - payload: Predefined payload functions
-- hide: Hide data in images
-- extract: Extract data from images
+- stego: Hide & extract data from images
 """
 
 from .payload import PAYLOADS
-from .hide import hide_data
-from .extract import extract_data
+from .stego import hide_message, extract_message
 
-__version__ = "2.0"
-__all__ = ["PAYLOADS", "hide_data", "extract_data"]
+__version__ = "1.0"
+__all__ = ["PAYLOADS", "hide_message", "extract_message"]
